7f3071825bf1a85bba3bae99abdf547b52d31122
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-09 13:36:54 +0300

    Created several functions for creating user account, checking the initialization and checking if the credentials are saved.

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -9,3 +9,24 @@ class TestUser(unittest.TestCase):
     Args:
     unittest.TestCase: helps in creating test cases
     '''
+    
+    def setUp(self):
+        '''
+        Function to create a user account before each test
+        '''
+        self.new_user = User('Olive', 'Wangui', 'lil2000')
+        
+    def test__init__(self):
+        '''
+        Test to see if the initialization of the user instance is well done
+        '''
+        self.assertEqual(self.new_user.first_name, 'Olive')
+        self.assertEqual(self.new_user.last_name, 'Wangui')
+        self.assertEqual(self.new_user.password, 'lil2000')
+        
+    def test_save_user(self):
+        '''
+        Test to check if the new users information is saved as required
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
