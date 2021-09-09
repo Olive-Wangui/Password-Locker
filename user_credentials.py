303dc60de2310ba2245e3a2bf30ac2b187f0b761
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-09 12:44:49 +0300

    Created a function for generating password for a credential.

--- user_credentials.py
+++ user_credentials.py
@@ -61,4 +61,11 @@ class Credential:
         '''
         # Gloabal users_list
         Credential.credentials_list.append(self)
+        
+    def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+        '''
+        Function to genrate an 8 character password for a credential
+        '''
+        gen_pass = ''.join(random.choice(char) for _ in range(size))
+        return gen_pass
     
