241df4181401436d6c487b9b3acb47397433d58f
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-09-09 12:08:45 +0300

    Addition of code of creating a class for creating user accounts, methods of defining the properties and a function to save the created user instance.

--- user_credentials.py
+++ user_credentials.py
@@ -2,3 +2,27 @@ import pyperclip
 import random
 import string
 
+# General Variables
+global users_list
+class User:
+    '''
+    Class to create user accounts and save information
+    '''
+    # Class Variables
+    # Global users_list
+    users_list = []
+    def __init__(self, first_name, last_name, password):
+        '''
+        Method to define the properties for each user object will hold
+        '''
+        
+        # Instance Variables
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        
+        def save_user(self):
+            '''
+            Function to save a newly created user instance
+            '''
+            User.users_list.append(self)
